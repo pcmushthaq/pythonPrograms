549d712b5fd2f8734a9e903ff042163765290747
pcmushthaq2 <pcmushthaq2@gmail.com>
2021-04-13 21:08:40 +0530

    twoSum Problem

---
+++ twoSum.py
@@ -0,0 +1,16 @@
+# Find if there are two elements in the array which will
+# give the target sum
+# If there are none return false, If there is return the numbers
+def twoSum(arr1, target):
+    elements = {}
+    for item in arr1:
+        if target-item in elements:
+            return [item, target-item]
+        else:
+            elements[item] = item
+    return False
+
+
+target = 10
+sampleArray = [1, 4, 2, 5, 5, 2]
+print(twoSum(sampleArray, target))
