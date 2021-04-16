46e502c49dc3a137c2ffee9dc740a64135b8ed20
pcmushthaq2 <pcmushthaq2@gmail.com>
2021-04-16 17:44:23 +0530

    reverse linked list

---
+++ reverseLinkedList.py
@@ -0,0 +1,52 @@
+from linkedList import LinkedList, Node
+
+
+def reverseLList(linkedList: LinkedList):
+    # Better solution
+    # Single Loop, Reverse pointers and replace head and tail
+    head = linkedList.head
+
+    # This step is also crucial
+    linkedList.tail = head
+    print(linkedList.tail.next)
+    first = head
+    second = head.next
+    while(second is not None):
+        temp = second.next
+        # This is the key point
+        # We are just reversing the pointers
+        second.next = first
+        # These are only for continuing the loop
+        first = second
+        second = temp
+    print(linkedList.tail.next)
+    print(first.next)
+    linkedList.head.next = None
+    linkedList.head = first
+
+
+def reverseLinkedList(linkedList: LinkedList):
+    # Brute Force Solution
+    currentNode = linkedList.head
+    values = []
+
+    while currentNode is not None:
+        values.append(currentNode.value)
+        currentNode = currentNode.next
+    for i in range(linkedList.length):
+        node = linkedList.traverseToIndex(i)
+        node.value = values[linkedList.length-1-i]
+    # This is an O(n+n) solution?
+
+
+linkedList = LinkedList(Node(1))
+testList = [2, 3, 4, 5, 6, 7, 8, 9]
+for num in testList:
+    linkedList.append(Node(num))
+linkedList.printList()
+reverseLList(linkedList)
+linkedList.printList()
+print(linkedList.head.value)
+
+reverseLinkedList(linkedList)
+linkedList.printList()
