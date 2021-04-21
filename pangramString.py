983f0539e1ecdbe7d5f1a7d39677814c09dba3b1
pcmushthaq <pcmushthaq@gmail.com>
2021-04-21 21:07:35 +0530

    pangram string

---
+++ pangramString.py
@@ -0,0 +1,23 @@
+# A pangram is a sentence where every letter of the
+# English alphabet appears at least once.
+
+# Given a string sentence containing only lowercase
+#  English letters, return true if sentence is a pangram,
+#  or false otherwise.
+
+def checkIfPangram(self, sentence: str) -> bool:
+    uniqueCount = 0
+    hashMap = {}
+    for letter in sentence:
+        if letter in hashMap:
+            continue
+        else:
+            hashMap[letter] = True
+            uniqueCount += 1
+    if uniqueCount == 26:
+        return True
+    else:
+        return False
+
+# "thequickbrownfoxjumpsoverthelazydog"
+# "leetcode"
